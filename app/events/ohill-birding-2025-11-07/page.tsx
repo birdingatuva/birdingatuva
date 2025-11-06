b263a6ca254360ba760676663e4a039e08df8011
Brian <rpt2fx@virginia.edu>
2025-11-05 23:59:52 -0500

    updated trip description

--- app/events/ohill-birding-2025-11-07/page.tsx
+++ app/events/ohill-birding-2025-11-07/page.tsx
@@ -47,13 +47,30 @@ export default function OhillBirdingEvent() {
                   </div>
                 </div>
                 <div className="mb-6 text-muted-foreground">
-                  <p>Come birding with us on Ohill! We'll meet at Slaughter, head out to the trails, and hopefully see a good number of birds. All skill levels are welcome! Bring binoculars if you have any, and if you don't you can still enjoy birding with the Merlin ID app which lets you identify birds by sound and photo. See you there!</p>
+                  <p className="mb-4">Hello everyone! It was great seeing some of you at the interest meeting on Wednesday night! If you didn’t get a chance to come, stop by on Tuesday in New Cabell 232. Before then, though, we wanted to do some group birding!!</p>
+
+                  <p className="mb-4">We will meet at 7:00 at the Slaughter Recreation Center and leave around 7:05 to start our bird walk. We should be back at the slaughter around 9:15.</p>
+
+                  <p className="mb-4">On the walk, you can expect to see mostly resident species, which will allow new birders to learn about the species they are likely to encounter, and will give experienced birders a chance to brush up on some ID skills. This is a low-intensity trip, so we hope that everyone who can will attend.</p>
+
+                  <p className="mb-4">Here are some recommendations of pre-trip actions:</p>
+                  <ul className="list-disc pl-6 mb-4">
+                    <li><strong>Fill out the attendance form below</strong> (this is required)</li>
+                    <li>Download the <strong>Merlin bird ID app</strong> on your phone, as well as the Virginia bird pack</li>
+                    <li>Bring a full water bottle</li>
+                    <li>Grab some binoculars, if you have them (if not, they’re definitely not needed)</li>
+                    <li>Wear warm clothes!! It’s gonna be a chilly 42 degrees, so be sure to dress for the weather</li>
+                  </ul>
+
+                  <p className="mb-4">Hope to see you all there! If you have any questions, comments, or concerns, send a message on the GroupMe, DM one of the Execs, or send an email to <strong>birdingatuva@gmail.com</strong></p>
                 </div>
                 <div className="mb-6">
                   <div className="bg-white/90 rounded-xl shadow-md border border-gray-100 p-6">
-                    <h3 className="font-display text-2xl font-bold text-black mb-4">Sign Up</h3>
+                    <h3 className="font-display text-2xl font-bold text-black mb-4">
+                      <a href="https://docs.google.com/forms/d/e/1FAIpQLScb7WsWg5hrHRK5OwsLxtzQ4B9BXD3kh-8y1P9BiS6zKx4JQg/viewform?usp=dialog" target="_blank" rel="noopener noreferrer">Sign Up</a>
+                    </h3>
                     <iframe
-                      src="https://forms.gle/JG4onYKPjaUWLnX57"
+                      src="https://docs.google.com/forms/d/e/1FAIpQLScb7WsWg5hrHRK5OwsLxtzQ4B9BXD3kh-8y1P9BiS6zKx4JQg/viewform?usp=dialog"
                       width="100%"
                       height="600"
                       frameBorder="0"
